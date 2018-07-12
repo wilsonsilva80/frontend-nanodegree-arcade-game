d05f199bf2080332b61c04ddda6099719f33ee2b
Wilson Silva <wilsonsilva80@gmail.com>
2018-07-12 16:00:40 +0100

    player side collisions & win

--- js/app.js
+++ js/app.js
@@ -1,14 +1,14 @@
-// TODO: update player method can be used to check if player has won the game and an alert pops up.
 // TODO: create a startPos to Player so that there's no need to instantiate it when creating it
-// TODO: create a startPos to Enemie with a random start position on the y axis
-// TODO: create a speed parameter to enemie and randomize it when instantiating it
+// TODO: create a startPos to Enemy with a random start position on the y axis
+// TODO: create a speed parameter to enemy and randomize it when instantiating it
 
 // Enemies our player must avoid
-var Enemy = function(x, y) {
+var Enemy = function(x, y, speed = 20) {
     // Variables applied to each of our instances go here,
     // we've provided one for you to get started
     this.x = x;
     this.y = y;
+    this.speed = speed;
     // The image/sprite for our enemies, this uses
     // a helper we've provided to easily load images
     this.sprite = 'images/enemy-bug.png';
@@ -40,7 +40,24 @@ var Player = function (x, y) {
 }
 
 Player.prototype.update = function() {
-    //already written at the TODO
+    //bottom side
+    if (this.y >= 400) {
+        this.y = 400;
+    }
+    //right side
+    if (this.x >= 420) {
+        this.x = 420;
+    }
+    //upper side
+    if (this.y <= -40) {
+        this.y = -40;
+    }
+    //left side
+    if (this.x <= -15) {
+        this.x = -15;
+    }
+    //check if game Won
+    gameWon();
 }
 
 Player.prototype.render = function() {
@@ -64,7 +81,16 @@ Player.prototype.handleInput = function (keyPress) {
     if (keyPress == 'down') {
         this.y += 15;
     }
+    console.log(`x: ${this.x} \n y: ${this.y}`);
 
+}
+
+const gameWon = () => {
+    if (player.y <= -25) {
+        console.log('You won the game');
+        player.x = 200;
+        player.y = 400;
+    }
 }
 
 // Now instantiate your objects.
@@ -78,7 +104,7 @@ player = new Player(200, 400);
 
 // This listens for key presses and sends the keys to your
 // Player.handleInput() method. You don't need to modify this.
-document.addEventListener('keyup', function(e) {
+document.addEventListener('keydown', function(e) {
     var allowedKeys = {
         37: 'left',
         38: 'up',
